5e9a635ffab9df38209be27370b22e6157a2f4ca
Engramar Bollas <engramar.bollas@gmail.com>
2022-03-05 10:23:42 +1100

    The Hexagon Feb 2022

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import sqlite3
 
 def connect_db():
     #sql = sqlite3.connect('C:\Projects\codesydneywebsite\codesydney.db')
-    #sql = sqlite3.connect('/home/engramar/codesydneywebsite/codesydney.db')
+    #sql = sqlite3.connect('/home/codesydney/codesydneywebsite/codesydney.db')
     sql = sqlite3.connect('/Users/engramarbollas/Projects/codesydneywebsite/codesydney.db')
     sql.row_factory = sqlite3.Row
     return sql
